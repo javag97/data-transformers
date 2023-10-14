05b2d7cd9ee618261fb4fff0975930f0821b7175
Javier Garcia <jgarcia@gitlab.com>
2023-10-14 15:42:19 -0700

    Change to CSV to JSON

--- src/csv_to_json.py
+++ src/csv_to_json.py
@@ -1,3 +1,11 @@
-def csv_to_df(csv):
-  return csv
+"""Working with CSV files using pandas."""
+import pandas
+import json
+
+def csv_to_json(csv):
+  """Assumes database has been loaded into a CSV, then transforms into a python dictionary."""
+  df = pandas.read_csv(csv)
+  results = df.to_json(orient='records')
+  parsed = json.loads(results)
+  return parsed
   
--- tests/test_csv_to_json.py
+++ tests/test_csv_to_json.py
@@ -1,9 +1,9 @@
 import pytest
-from src.csv_to_json import csv_to_df
+from src.csv_to_json import csv_to_json
 
 @pytest.fixture
 def sample_dict():
     return {"Python": 3}
 
 def test_simple_data_csv_df(sample_dict):
-  assert csv_to_df(sample_dict) == {'Python': 3}
+  assert csv_to_json(sample_dict) == {'Python': 3}
