20394d3bc3cc1a365e86b20e1e2fd6079d6ed4cd
Javier Garcia <jgarcia@gitlab.com>
2023-10-17 20:23:46 -0700

    Add comments, fix bugs

--- src/csv_to_json.py
+++ src/csv_to_json.py
@@ -4,16 +4,34 @@ import json
 
 
 def is_df(df):
+    """Check if the input is a pandas DataFrame.
+    
+    Parameters:
+        df: The object to check.
+    
+    Returns:
+        bool: True if df is a pandas DataFrame, otherwise raises a ValueError.
+    """
     if not isinstance(df, pd.DataFrame):
         raise ValueError("Expected a DataFrame, but received a different type.")
-        return False
     else:
         return True
     
 
 def df_to_json(*args, db_schema=None):
-    """Assumes database has been loaded into a df, then transforms into a JSON file."""
-
+    """Converts one or multiple pandas DataFrames to JSON format.
+    
+    If a single dataframe is passed, it's converted to a JSON array of records.
+    If multiple dataframes are passed, they are merged together according to the database schema.
+    This is not done yet.
+    
+    Parameters:
+        *args: One or more pandas DataFrames to convert.
+        db_schema (str): An optional parameter that could be used to customize the JSON structure based on SQL schema relationships.
+    
+    Returns:
+        list: A list containing the JSON representation of each DataFrame.
+    """
     if len(args) == 1:
         if is_df(args[0]):
             results = args[0].to_json(orient='records')
@@ -26,11 +44,9 @@ def df_to_json(*args, db_schema=None):
     data = []
 
     for df in args:
-        if is_df(df):
-            return None
-        results = df.to_json(orient='records')
-        parsed = json.loads(results)
-        data.append(parsed)
+        if not is_df(df):
+            raise ValueError("One of your dataframes is not valid.")
+        # todo: Implement merging logic based on db_schema
     
     return data
   
