bc14d7fd6ba4eaac0dd74ab41273887812ae50ae
Javier Garcia <jgarcia@gitlab.com>
2023-10-19 15:07:04 -0700

    Refactor schema

--- src/csv_to_json.py
+++ src/csv_to_json.py
@@ -18,6 +18,26 @@ def is_df(df):
     else:
         return True
     
+def valdiate_schema_csv(schema):
+    """
+    Validates the schema.csv file for its structure and content.
+
+    Parameters:
+        filepath (schema): A pandas dataframe representation of a schema
+
+    Returns:
+        bool: True if the CSV is valid, False otherwise.
+    """
+
+    expected_columns = [
+        "conname", "conrelid", "conrelid_file", "attname",
+        "confrelid_file", "confrelid",
+    ]
+
+    
+    
+    
+    
 
 def df_to_json(*args, db_schema=None):
     """Converts one or multiple pandas DataFrames to JSON format.
@@ -51,33 +71,38 @@ def df_to_json(*args, db_schema=None):
     if len(args) == 2:
         df1 = args[0]
         df2 = args[1]
-        df1_name = db_schema['conrelid'][0] #name of table the constraint is on, foreign key
-        df2_name = db_schema['confrelid'][0] #name of refrenced table
+        
+        df1_name = db_schema['conrelid'][0] # Name of the table with the foreign key constraint.
+        df2_name = db_schema['confrelid'][0] # Name of the referenced table.
 
-        shared_column = db_schema['attname'][0] #name of the foreign key in one table
-        if shared_column != db_schema['attname.1'][0]: 
-            df2 = df2.rename(columns={db_schema['attname.1'][0]: shared_column}) #if the attnames are different, we will rename them to be the same 
+        fk_col = db_schema['fk_column'][0] # Foreign key column in the table with the constraint.
+        pk_col = db_schema['pk_column'][0] # Primary key column in the referenced table.
 
-        df1_columns = list(np.setdiff1d(df1.columns, [shared_column])) #columns in table besides shared column
-        df2_columns = list(np.setdiff1d(df2.columns, [shared_column]))
+        # Rename the column in df2 to match the fk_col in df1, if they're different.
+        if fk_col != pk_col:
+            df2 = df2.rename(columns={pk_col: fk_col})
 
-        dup_col_name = np.intersect1d(df2_columns, df1_columns) #if both dataframes have columns with the same name that's not a foreign key
+        df1_columns = list(np.setdiff1d(df1.columns, [fk_col])) # Columns in df1 besides the foreign key.
+        df2_columns = list(np.setdiff1d(df2.columns, [fk_col]))
+
+        # If both dataframes have columns with the same name that's not the foreign key.
+        dup_col_name = np.intersect1d(df2_columns, df1_columns)
         if len(dup_col_name) != 0:
             for col in dup_col_name:
-                df1 = df1.rename({col: col+'_x'}, axis=1) #renaming both columns
+                df1 = df1.rename({col: col+'_x'}, axis=1)
                 df2 = df2.rename({col: col+'_y'}, axis=1)
-                df1_columns = list(np.setdiff1d(df1.columns, [shared_column])) 
-                df2_columns = list(np.setdiff1d(df2.columns, [shared_column]))
+                df1_columns = list(np.setdiff1d(df1.columns, [fk_col]))
+                df2_columns = list(np.setdiff1d(df2.columns, [fk_col]))
 
-        df = df1.merge(df2, on=shared_column)  
-        all_cols = [shared_column] + df2_columns 
+        df = df1.merge(df2, on=fk_col)
+        all_cols = [fk_col] + df2_columns 
         temp = (df.groupby(all_cols)[df1_columns]
                 .apply(lambda x: x.to_dict('records'))
-                .reset_index(name = df1_name)
+                .reset_index(name=df1_name)
                 .to_json(orient='records')
                 )
         parsed = json.loads(temp)
-        
+
         final = json.dumps({df2_name: parsed})
         with open("results.json", "w") as outfile: #fix: write out to results folder
             outfile.write(final)
