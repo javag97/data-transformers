f2cc318a64434829b5b4e0177887844df29eb97c
Javier Garcia <jgarcia@gitlab.com>
2023-10-19 16:03:39 -0700

    I think i broke it :(

--- src/csv_to_json.py
+++ src/csv_to_json.py
@@ -49,7 +49,39 @@ def valdiate_schema_csv(schema):
     if not set(schema['fk_column']).issubset(schema['pk_column']) or not set(schema['pk_column']).issubset(schema['fk_column']):
         raise ValueError("Mismatch between foreign key and primary key columns.")
     
-    return True    
+    return True
+
+def merge_df(df1, df2, db_schema):    
+    df1_name = db_schema['conrelid'][0] # Name of the table with the foreign key constraint.
+
+    fk_col = db_schema['fk_column'][0] # Foreign key column in the table with the constraint.
+    pk_col = db_schema['pk_column'][0] # Primary key column in the referenced table.
+
+    # Rename the column in df2 to match the fk_col in df1, if they're different.
+    if fk_col != pk_col:
+        df2 = df2.rename(columns={pk_col: fk_col})
+
+    df1_columns = list(np.setdiff1d(df1.columns, [fk_col])) # Columns in df1 besides the foreign key.
+    df2_columns = list(np.setdiff1d(df2.columns, [fk_col]))
+
+    # If both dataframes have columns with the same name that's not the foreign key.
+    dup_col_name = np.intersect1d(df2_columns, df1_columns)
+    if len(dup_col_name) != 0:
+        for col in dup_col_name:
+            df1 = df1.rename({col: col+'_x'}, axis=1)
+            df2 = df2.rename({col: col+'_y'}, axis=1)
+            df1_columns = list(np.setdiff1d(df1.columns, [fk_col]))
+            df2_columns = list(np.setdiff1d(df2.columns, [fk_col]))
+
+    df = df1.merge(df2, on=fk_col)
+    all_cols = [fk_col] + df2_columns 
+    temp = (df.groupby(all_cols)[df1_columns]
+            .apply(lambda x: x.to_dict('records'))
+            .reset_index(name=df1_name)
+            .to_json(orient='records')
+            )
+    parsed = json.loads(temp)
+
 
 def df_to_json(*args, db_schema=None):
     """Converts one or multiple pandas DataFrames to JSON format.
@@ -84,41 +116,10 @@ def df_to_json(*args, db_schema=None):
         # raise ValueError("Your schema is not valid.")
     
     if len(args) == 2:
-        df1 = args[0]
-        df2 = args[1]
-        
-        df1_name = db_schema['conrelid'][0] # Name of the table with the foreign key constraint.
+        parsed = merge_df(args[0], args[1], db_schema)
         df2_name = db_schema['confrelid'][0] # Name of the referenced table.
-
-        fk_col = db_schema['fk_column'][0] # Foreign key column in the table with the constraint.
-        pk_col = db_schema['pk_column'][0] # Primary key column in the referenced table.
-
-        # Rename the column in df2 to match the fk_col in df1, if they're different.
-        if fk_col != pk_col:
-            df2 = df2.rename(columns={pk_col: fk_col})
-
-        df1_columns = list(np.setdiff1d(df1.columns, [fk_col])) # Columns in df1 besides the foreign key.
-        df2_columns = list(np.setdiff1d(df2.columns, [fk_col]))
-
-        # If both dataframes have columns with the same name that's not the foreign key.
-        dup_col_name = np.intersect1d(df2_columns, df1_columns)
-        if len(dup_col_name) != 0:
-            for col in dup_col_name:
-                df1 = df1.rename({col: col+'_x'}, axis=1)
-                df2 = df2.rename({col: col+'_y'}, axis=1)
-                df1_columns = list(np.setdiff1d(df1.columns, [fk_col]))
-                df2_columns = list(np.setdiff1d(df2.columns, [fk_col]))
-
-        df = df1.merge(df2, on=fk_col)
-        all_cols = [fk_col] + df2_columns 
-        temp = (df.groupby(all_cols)[df1_columns]
-                .apply(lambda x: x.to_dict('records'))
-                .reset_index(name=df1_name)
-                .to_json(orient='records')
-                )
-        parsed = json.loads(temp)
-
         final = json.dumps({df2_name: parsed})
+        print(final)
         with open("results.json", "w") as outfile: #fix: write out to results folder
             outfile.write(final)
         return parsed
