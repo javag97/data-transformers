016c54187b232c70edc760cb4e0ec433ced0a306
Javier Garcia <jgarcia@gitlab.com>
2023-10-19 14:38:09 -0700

    simplify if statements slightly

--- src/csv_to_json.py
+++ src/csv_to_json.py
@@ -24,7 +24,7 @@ def df_to_json(*args, db_schema=None):
     
     If a single dataframe is passed, it's converted to a JSON array of records.
     If multiple dataframes are passed, they are merged together according to the database schema.
-    This is not done yet.
+    This works for two dataframes
     
     Parameters:
         *args: One or more pandas DataFrames to convert.
@@ -33,64 +33,53 @@ def df_to_json(*args, db_schema=None):
     Returns:
         list: A list containing the JSON representation of each DataFrame.
     """
+    for df in args:
+        if not is_df(df):
+            raise ValueError("One of your dataframes is not valid.")
+
     if len(args) == 1:
         if db_schema is not None:
             raise ValueError("No schema needed with one table.")
-
-        if is_df(args[0]):
-            results = args[0].to_json(orient='records')
-            parsed = json.loads(results)
-            return parsed
+        results = args[0].to_json(orient='records')
+        parsed = json.loads(results)
+        return parsed
         
     if db_schema is None:
         raise ValueError("Expected a db_schema, but received None.")
 
     
     if len(args) == 2:
-        if is_df(args[0]) and is_df(args[1]):
-            df1 = args[0]
-            df2 = args[1]
-            df1_name = db_schema['conrelid'][0] #name of table the constraint is on, foreign key
-            df2_name = db_schema['confrelid'][0] #name of refrenced table
+        df1 = args[0]
+        df2 = args[1]
+        df1_name = db_schema['conrelid'][0] #name of table the constraint is on, foreign key
+        df2_name = db_schema['confrelid'][0] #name of refrenced table
 
-            shared_column = db_schema['attname'][0] #name of the foreign key in one table
-            if shared_column != db_schema['attname.1'][0]: 
-                df2 = df2.rename(columns={db_schema['attname.1'][0]: shared_column}) #if the attnames are different, we will rename them to be the same 
+        shared_column = db_schema['attname'][0] #name of the foreign key in one table
+        if shared_column != db_schema['attname.1'][0]: 
+            df2 = df2.rename(columns={db_schema['attname.1'][0]: shared_column}) #if the attnames are different, we will rename them to be the same 
 
-            df1_columns = list(np.setdiff1d(df1.columns, [shared_column])) #columns in table besides shared column
-            df2_columns = list(np.setdiff1d(df2.columns, [shared_column]))
+        df1_columns = list(np.setdiff1d(df1.columns, [shared_column])) #columns in table besides shared column
+        df2_columns = list(np.setdiff1d(df2.columns, [shared_column]))
 
-            dup_col_name = np.intersect1d(df2_columns, df1_columns) #if both dataframes have columns with the same name that's not a foreign key
-            if len(dup_col_name) != 0:
-                for col in dup_col_name:
-                    df1 = df1.rename({col: col+'_x'}, axis=1) #renaming both columns
-                    df2 = df2.rename({col: col+'_y'}, axis=1)
-                    df1_columns = list(np.setdiff1d(df1.columns, [shared_column])) 
-                    df2_columns = list(np.setdiff1d(df2.columns, [shared_column]))
+        dup_col_name = np.intersect1d(df2_columns, df1_columns) #if both dataframes have columns with the same name that's not a foreign key
+        if len(dup_col_name) != 0:
+            for col in dup_col_name:
+                df1 = df1.rename({col: col+'_x'}, axis=1) #renaming both columns
+                df2 = df2.rename({col: col+'_y'}, axis=1)
+                df1_columns = list(np.setdiff1d(df1.columns, [shared_column])) 
+                df2_columns = list(np.setdiff1d(df2.columns, [shared_column]))
 
-            df = df1.merge(df2, on=shared_column)  
-            all_cols = [shared_column] + df2_columns 
-            temp = (df.groupby(all_cols)[df1_columns]
-                    .apply(lambda x: x.to_dict('records'))
-                    .reset_index(name = df1_name)
-                    .to_json(orient='records')
-                   )
-            parsed = json.loads(temp)
-            
-            final = json.dumps({df2_name: parsed})
-            with open("results.json", "w") as outfile: #fix: write out to results folder
-                outfile.write(final)
-            return final
-    
-    if db_schema == None:
-        raise ValueError("Expected a db_schema, but received None.")
-
-    data = []
-
-    for df in args:
-        if not is_df(df):
-            raise ValueError("One of your dataframes is not valid.")
-        # todo: Implement merging logic based on db_schema
-    
-    return data
-    
+        df = df1.merge(df2, on=shared_column)  
+        all_cols = [shared_column] + df2_columns 
+        temp = (df.groupby(all_cols)[df1_columns]
+                .apply(lambda x: x.to_dict('records'))
+                .reset_index(name = df1_name)
+                .to_json(orient='records')
+                )
+        parsed = json.loads(temp)
+        
+        final = json.dumps({df2_name: parsed})
+        with open("results.json", "w") as outfile: #fix: write out to results folder
+            outfile.write(final)
+        return parsed
+        
